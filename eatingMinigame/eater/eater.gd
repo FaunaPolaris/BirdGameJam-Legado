ff4de502c83046b7888ec28e8d462fa0154c2020
Fauna Polaris <max.maxpinheiro@gmail.com>
2025-07-26 15:51:04 -0300

    peck timeout

--- eatingMinigame/eater/eater.gd
+++ eatingMinigame/eater/eater.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-var	speed	: Vector2 = Vector2(20000, 0)
+var has_peck	: bool = true
+var	speed		: Vector2 = Vector2(20000, 0)
 
 func	_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
@@ -9,16 +10,19 @@ func	_process(delta: float) -> void:
 	elif Input.is_action_pressed("ui_right"):
 		velocity = Vector2.RIGHT * speed * delta
 		move_and_slide()
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") and has_peck:
+		has_peck = false
 		$collision.disabled = false
 		$art.set_offset(Vector2(0, 64))
+		$peckTimeout.start()
 		$peckTime.start()
-
 
 func _on_peck_time_timeout() -> void:
 	$collision.disabled = true
 	$art.set_offset(Vector2(0, 32))
 
-
 func _on_clock_times_up() -> void:
 	get_tree().change_scene_to_file("res://canonGame/level/level.tscn")
+
+func _on_peck_timeout_timeout() -> void:
+	has_peck = true
