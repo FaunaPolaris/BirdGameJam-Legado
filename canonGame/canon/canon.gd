5061f18dab48782efdceb27bd7960b692aab0029
thomaspixelart <142053591+thomaspixelart@users.noreply.github.com>
2025-07-26 15:50:10 -0300

    Cannon Tween Bounce

--- canonGame/canon/canon.gd
+++ canonGame/canon/canon.gd
@@ -7,6 +7,7 @@ var	min_angle		: int = 0
 var	canon_speed		: int = 50
 var	has_shot		: bool = false
 
+
 func _process(delta : float):
 	if Input.is_action_pressed("ui_up") and $mouth.global_rotation_degrees > min_angle:
 		$mouth.global_rotation_degrees -= canon_speed * delta
@@ -16,7 +17,10 @@ func _process(delta : float):
 		$CannonWheel.global_rotation_degrees -= canon_speed * delta
 	if Input.is_action_just_pressed("ui_accept") and !has_shot:
 		var new_main = main_character.instantiate()
-		$animation.play("shot")
+		#$animation.play("shot")
+		var tween:Tween = self.create_tween();
+		tween.tween_property($mouth, "scale", Vector2.ONE * 4.3, 0.1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC);
+		tween.tween_property($mouth, "scale", Vector2.ONE * 4.0, 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC);
 		has_shot = true
 		new_main.position = $mouth/tip.global_position
 		new_main.original_pos = new_main.position
